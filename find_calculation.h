d39101e84a5ad27e5cc960e560a462968c38cda8
SPARSH7283 <162866502+SPARSH7283@users.noreply.github.com>
2024-03-11 13:51:32 +0530

    find_calculation.h

--- find_calculation.h
+++ find_calculation.h
@@ -3,14 +3,32 @@
 
 namespace mathFunctions{
     double calculateSquareRoot(double x){
-        return x*x;
+    double s= 0;
+    double e = x;
+    double mid = s + (e-s)/2;
+    double ans = -1;
+    while(s<=e){
+        if(mid*mid == x){
+            return mid;
+        }
+        else if(mid*mid>=x){
+            e = mid-1;
+        }
+        else{
+            ans = mid;
+            s = mid+1;
+        }
+        mid = s + (e-s)/2;
     }
+    return ans;
+
+}
     double calculateCube(double x){
         return x*x*x;
     }
     double findPow(double n1,double n2){
         double x=n1;
-        for(int i=1; i<n2; i++){
+        for(double i=1; i<n2; i++){
             x*=n1;
         }
         return x;
